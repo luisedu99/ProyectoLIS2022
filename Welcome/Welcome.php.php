4a8875006bb2297eaa96f5b73edb2920575c09bf
Ernesto0091 <56093451+Ernesto0091@users.noreply.github.com>
2022-03-25 16:03:08 -0600

    Add files via upload

---
+++ Welcome/Welcome.php.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Journal Clinical</title>
+</head>
+<body>
+    <header class="head">
+        <nav class="navbar navbar-light bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" style="color: white;" id="Name">Clinical Journal - Nos guiamos por la salud.</a>
+                <form class="d-flex">
+                <img src="img/Logo.jfif" alt="" width="190px" height="100px">
+                </form>
+                
+            </div>
+        </nav>
+    </header>
+    <div class="nav">
+        <nav class="nav">
+            <a class="nav-link active" aria-current="page" href="pantalla.php" style="color: white;">Journal Clinical</a>
+            <a class="nav-link" href="Login.php" style="color: white;">Login</a>
+            <a class="nav-link" href="Registrarse.php" style="color: white;">Regístrate</a>
+            <a class="nav-link " href="#info" style="color: white;">Contáctanos</a>
+        </nav>
+
+    </div>
+    <br>
+    <div class="slider">
+        <div class="container justify-content-center">
+        <div id="carousel-1" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-1" data-slide-to="1"></li>
+            <li data-target="#carousel-1" data-slide-to="2"></li>
+            <li data-target="#carousel-1" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active"> <!-- la clase es carousel-item -->
+            <img src="img/2.jpg" class="d-block w-100" alt="" height="600px">
+            </div>
+            <div class="carousel-item">
+            <img src="img/3.jpg" class="d-block w-100" alt="" height="600px">
+            </div>
+            <div class="carousel-item">
+            <img src="img/4.jpg" class="d-block w-100" alt="" height="600px">
+            </div>
+            <div class="carousel-item">
+            <img src="img/5.jpg" class="d-block w-100" alt="" height="600px">
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+  <br><br>
+  <section class="Especialidades">
+        <h2>Nuevas Especialidades:</h2>
+        <hr>
+        <br>
+       <div class="container1">
+        <div class="row align-items-center">
+            <div class="col">
+            <h3>Dignóstico</h3>
+            <img src="img/diagnostico.png" alt=""><br><br>
+            <div class="line"></div>
+            </div>
+            <div class="col">
+            <h3>Otorrinolaringología </h3>
+            <img src="img/otorrino.jpg" alt=""><br><br>
+            <div class="line"></div>
+            </div>
+            <div class="col">
+            <h3>Pediatria</h3>
+            <img src="img/pediatria.jpg" alt=""><br><br>
+            <div class="line"></div>
+            </div>
+        </div>
+       </div>
+  </section>
+  <br><br>
+    <div class="info" id="info">
+        <div class="container">
+            <h2 style="color: white;">Contáctanos:</h2>
+            <hr>
+            <div class="row align-items-center">
+                <div class="col">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50s" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                </svg>
+                <h5 style="color:white;">Escribenos a nuestro E-Mail</h5>
+                </div>
+                <div class="col">
+                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-telephone-outbound" viewBox="0 0 16 16">
+                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                <h6 style="color:white;">Tanbien puesdes llamarnos a:</h6>
+                <h6 style="color: white;"> +503 2020 - 4578</h6>
+                </div>
+                <div class="col">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                </svg>
+                <h6 style="color: white;">Tambien puedes escribirnos a nuestro WhastApp</h6>
+                </div>
+                <hr>
+                <br>
+            </div>
+        </div>
+    </div>
+    <br><BR><BR></BR>
+    <hr>
+    <footer class="footer">
+    <nav class="navbar navbar-light bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" style="color: white;" id="Name">Clinical Journal. © Todos Los Derechos Reservados 2022 </a>
+                <form class="d-flex">
+                <img src="img/Logo.jfif" alt="" width="190px" height="100px">
+                </form>
+                
+            </div>
+        </nav>
+    </footer>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
